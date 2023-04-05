1c123bb7f1b236aa14f672e0b2ef27fcc0011cc4
giovanyosorio <giovany.torres.799@gmail.com>
2023-04-05 12:51:55 -0500

    map reloaded

--- server/02-map.js
+++ server/02-map.js
@@ -1,6 +1,17 @@
-const letters=["a","b","c"]
+/*const letters=["a","b","c"]
 
 const array=letters.map((element)=>element+"++")
 
 console.log(array);
 console.log(letters);
+
+console.log(letters.map((element)=>element+"++"))
+*/
+function multiplyElements(array) {
+    // Tu código aquí 👈
+    console.log("arreglo",array.map((element)=>element*2));
+    console.log("array",array)
+ }
+
+ multiplyElements([2, 4, 5, 6, 8])
+ multiplyElements([1, 1, -2, -3])
